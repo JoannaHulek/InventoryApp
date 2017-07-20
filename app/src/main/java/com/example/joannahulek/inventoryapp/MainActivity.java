7119c816931f209cf11b2911a3347726d4299747
Joanna Hulek <joanna.hulek@gmail.com>
2017-07-20 20:44:33 +0200

    app layout

--- app/src/main/java/com/example/joannahulek/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/joannahulek/inventoryapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.joannahulek.inventoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        Button addNewButton = (Button) findViewById(R.id.add_new_product_button);
+        addNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddNewProductActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
